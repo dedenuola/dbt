7a6add1258d188fbd5338720863a671d6dd004c7
Oluwashina Dedenuola <dedenuolashina@gmail.com>
2024-10-16 10:12:54 +0000

    developed payment model to extract and fact check payments amount to date.

---
+++ models/staging/stripe/stg_stripe_payments.sql
@@ -0,0 +1,8 @@
+select id as customer_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+    --amount stored as cents to be converted to dollars
+    amount/100 as amount,
+    created as created_at
+from `dbt-dede.stripe.payments`
