06aff6700a9f586107128d6c2f12bbfbb6c42e88
Oluwashina Dedenuola <dedenuolashina@gmail.com>
2024-10-16 13:57:25 +0000

    Added source configuration to YAML for improved modularity in models

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -2,4 +2,4 @@ select
     id as customer_id,
     first_name,
     last_name
-from `dbt-dede.jaffle_shop.customers`
+from {{source('jaffle_shop', 'customers')}}
--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -3,4 +3,4 @@ select
     user_id as customer_id,
     order_date,
     status
-from `dbt-dede.jaffle_shop.orders`
+from {{source('jaffle_shop', 'orders')}}
